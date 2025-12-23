6b062dee71c7fee78736d280dd6359b3abbf9d9f
Punith_Gs <152014411+punithgs@users.noreply.github.com>
2025-12-23 15:44:19 +0530

    Add files via upload

---
+++ Factorial.java
@@ -0,0 +1,8 @@
+class Factorial {
+    public static void main(String[] args) {
+        int fact=1;
+        for(int i=1;i<=5;i++)
+            fact*=i;
+        System.out.println(fact);
+    }
+}
