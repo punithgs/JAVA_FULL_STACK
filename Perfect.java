8984bcf5a601cf65c76d88c1823f8077e6a9b84f
Punith_Gs <152014411+punithgs@users.noreply.github.com>
2025-12-25 11:23:45 +0530

    Add files via upload

---
+++ Perfect.java
@@ -0,0 +1,8 @@
+class Perfect {
+    public static void main(String[] args) {
+        int n=6, sum=0;
+        for(int i=1;i<n;i++)
+            if(n%i==0) sum+=i;
+        System.out.println(sum==n?"Perfect":"Not Perfect");
+    }
+}
