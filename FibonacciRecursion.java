9080ff3c4c06d4f9ecc90f3784884d6dc6461a70
Punith_Gs <152014411+punithgs@users.noreply.github.com>
2025-12-29 16:08:36 +0530

    Update FibonacciRecursion.java

--- FibonacciRecursion.java
+++ FibonacciRecursion.java
@@ -8,7 +8,7 @@ class FibonacciRecursion {
     }
 
     public static void main(String[] args) {
-        int n = 10; // number of terms
+        int n = 10; 
 
         for (int i = 0; i < n; i++) {
             System.out.print(fibonacci(i) + " ");
