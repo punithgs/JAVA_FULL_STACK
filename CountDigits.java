0131be65d66eee12b286808e3669307a443bd670
Punith_Gs <152014411+punithgs@users.noreply.github.com>
2026-01-01 09:59:02 +0530

    Update CountDigits.java

--- CountDigits.java
+++ CountDigits.java
@@ -1,6 +1,7 @@
 class CountDigits {
     public static void main(String[] args) {
-        int n=12345, count=0;
+        int n = 12345; 
+        int count = 0;
         while(n>0){
             count++;
             n/=10;
