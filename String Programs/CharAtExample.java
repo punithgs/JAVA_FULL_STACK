d2b0f79522c77804ee8fee9f449f044946f33bfb
Punith_Gs <152014411+punithgs@users.noreply.github.com>
2026-01-05 14:39:31 +0530

    Add files via upload

---
+++ String Programs/CharAtExample.java
@@ -0,0 +1,8 @@
+class CharAtExample {
+    public static void main(String[] args) {
+        String s = "Java";
+        char[] a = s.toCharArray();
+        for (char c : a)
+        System.out.println(c);
+    }
+}
