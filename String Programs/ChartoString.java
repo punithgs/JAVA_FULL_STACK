79300e32a18f027094318ba598aa90c4d1ff41bb
Punith_Gs <152014411+punithgs@users.noreply.github.com>
2026-01-06 15:24:35 +0530

    Add files via upload

---
+++ String Programs/ChartoString.java
@@ -0,0 +1,8 @@
+public class ChartoString {
+    public static void main(String[] args) {
+        
+        char[] a = {'j','a','v','a'};
+        String s = new String(a);
+        System.out.println(s);
+    }
+}
