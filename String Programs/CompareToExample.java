f26c2cfdab2481e8632e0eae2a0c410e6f53dc88
Punith_Gs <152014411+punithgs@users.noreply.github.com>
2026-01-07 20:54:49 +0530

    Add files via upload

---
+++ String Programs/CompareToExample.java
@@ -0,0 +1,7 @@
+class CompareToExample {
+    public static void main(String[] args) {
+        String a = "Apple";
+        String b = "Banana";
+        System.out.println(a.compareTo(b));
+    }
+}
