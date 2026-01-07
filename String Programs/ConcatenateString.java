8d05535f9979e5da427b2cd9f4ff430c126134e5
Punith_Gs <152014411+punithgs@users.noreply.github.com>
2026-01-07 20:55:20 +0530

    Add files via upload

---
+++ String Programs/ConcatenateString.java
@@ -0,0 +1,7 @@
+class ConcatenateString {
+    public static void main(String[] args) {
+        String a = "Hello";
+        String b = " World";
+        System.out.println(a.concat(b));
+    }
+}
