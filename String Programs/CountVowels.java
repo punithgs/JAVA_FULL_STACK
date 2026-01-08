093c1435bdc41efec2db8bb3b0387a84fa977f5d
Punith_Gs <152014411+punithgs@users.noreply.github.com>
2026-01-08 18:20:15 +0530

    Add files via upload

---
+++ String Programs/CountVowels.java
@@ -0,0 +1,12 @@
+class CountVowels {
+    public static void main(String[] args) {
+        String s = "Java";
+        int count = 0;
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c=='a'||c=='e'||c=='i'||c=='o'||c=='u')
+                count++;
+        }
+        System.out.println(count);
+    }
+}
