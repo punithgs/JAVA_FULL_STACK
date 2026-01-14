97e010699f6e0a685e5afa845f2208d195aec5be
Punith_Gs <152014411+punithgs@users.noreply.github.com>
2026-01-14 10:48:32 +0530

    Add files via upload

---
+++ String Programs/ReverseincharArray.java
@@ -0,0 +1,17 @@
+public class ReverseincharArray {
+    public static void main(String[] args) {
+        
+        String s = "developer";
+        char[] a = s.toCharArray();
+        int i = 0, j = a.length-1;
+        while (i<j) {
+            char temp = a[i];
+            a[i] = a[j];
+            a[j] = temp;
+            i++;
+            j--;
+        }
+        String s1 = new String(a);
+        System.out.println(s1);
+    }
+}
