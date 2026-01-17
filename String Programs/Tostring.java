5934970c5aeb05a13d18ff61ad2a09a7fc585bbf
Punith_Gs <152014411+punithgs@users.noreply.github.com>
2026-01-17 10:25:17 +0530

    Add files via upload

---
+++ String Programs/Tostring.java
@@ -0,0 +1,8 @@
+public class Tostring {
+    public static void main(String[] args) {
+        
+        char[] a = {'j','a','v','a'};
+        String s1 = new String(a);
+        System.out.println(s1);
+    }
+}
