0018a1b0fcca8152ef460f507298ba3a69ef8705
Punith_Gs <152014411+punithgs@users.noreply.github.com>
2026-01-17 10:03:48 +0530

    Add files via upload

---
+++ String Programs/Substring.java
@@ -0,0 +1,8 @@
+public class Substring {
+    public static void main(String[] args) {
+        
+        String s = "javadeveloper";
+        System.out.println(s.substring(4));
+        System.out.println(s.substring(4,7));
+    }
+}
