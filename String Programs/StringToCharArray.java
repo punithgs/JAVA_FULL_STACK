e3d22ba5910ac6bf42f8b2dbdd4276ff1adb7700
Punith_Gs <152014411+punithgs@users.noreply.github.com>
2026-01-17 10:03:23 +0530

    Add files via upload

---
+++ String Programs/StringToCharArray.java
@@ -0,0 +1,8 @@
+class StringToCharArray {
+    public static void main(String[] args) {
+        String s = "Java";
+        char[] ch = s.toCharArray();
+        for (char c : ch)
+            System.out.println(c);
+    }
+}
