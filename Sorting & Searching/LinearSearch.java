4556c3fe76f7048024dd9ddacca61ec810d456b1
Punith_Gs <152014411+punithgs@users.noreply.github.com>
2026-01-23 09:26:14 +0530

    Add files via upload

---
+++ Sorting & Searching/LinearSearch.java
@@ -0,0 +1,23 @@
+public class LinearSearch {
+    public static void main(String[] args) {
+        
+        int[] a = {4,8,1,2,5,9};
+        System.out.println(search(a, 4 ));
+        System.out.println(search(a, 6));
+    }
+
+    static int search (int[]a, int key)
+    {
+        for (int i=0; i<a.length; i++)
+        {
+            if(a[i]==key)
+                return i;
+        }
+        return -1;
+
+    }
+    
+}
+
+
+
