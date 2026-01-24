4300b50ecac20049ab77438c3df62c8ac38e7e30
Punith_Gs <152014411+punithgs@users.noreply.github.com>
2026-01-24 13:03:35 +0530

    Update MergeSort.java

--- Sorting & Searching/MergeSort.java
+++ Sorting & Searching/MergeSort.java
@@ -33,25 +33,6 @@ public class MergeSort {
         Merge(left,right,a);
     }
 
-    static void Merge (int[]a, int[]b, int[]c)
-    {
-        int i = 0, j = 0, k = 0;
 
-        while (i < a.length && j < b.length)
-        {
-            if (a[i] < b[j])
-            {
-                c[k++] = a[i++];
-            } else {
-                c[k++] = b[j++];
-            }
-        }
-        while (i<a.length) {
-            c[k++] = a[i++];
-        }
-        while (j<b.length) {
-            c[k++] = b[j++];
-            
-        }
     }
 }
