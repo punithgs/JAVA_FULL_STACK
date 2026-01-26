e89af3746b09a1c026b108f856141ce5829323fb
Punith_Gs <152014411+punithgs@users.noreply.github.com>
2026-01-26 09:44:57 +0530

    Add files via upload

---
+++ Sorting & Searching/QuickSort.java
@@ -0,0 +1,37 @@
+import java.util.Arrays;
+
+public class QuickSort {
+    public static void main(String[] args) {
+
+        int[]a = {5,8,1,2,9,12,4,23,7};
+        System.out.println(Arrays.toString(a));
+        sort(a,0,a.length-1);
+        System.out.println(Arrays.toString(a));
+    }
+    public void sort(int[] a)
+    {
+        sort (a,0,a.length-1);
+    }
+    public static void sort (int[]a, int start, int end)
+    {
+        if(start >= end)
+            return;
+        int i = start, j = end;
+        int pivot = a [(start + end) / 2 ];
+
+        while (i <= j) {
+            while (a[i] < pivot) i++ ;
+            while (a[j] > pivot) j-- ;
+            if (i <= j)
+                {
+                    int temp = a[i];
+                    a[i] = a[j];
+                    a[j] = temp;
+                    i++;
+                    j--;
+            }   
+            }
+            sort(a,start,j);
+            sort(a,i,end); 
+        }
+    }
