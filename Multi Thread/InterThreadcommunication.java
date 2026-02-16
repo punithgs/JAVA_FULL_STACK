884eb142891bccba650aa2c5310b48fa89b5f4ca
Punith_Gs <152014411+punithgs@users.noreply.github.com>
2026-02-16 11:12:28 +0530

    Add files via upload

---
+++ Multi Thread/InterThreadcommunication.java
@@ -0,0 +1,48 @@
+class Bank{
+    int bal;
+
+    public Bank(int bal)
+    {
+        this.bal = bal;
+    } 
+
+    public synchronized void deposit(int amount) {
+        System.out.println("Going to deposit");
+        bal += amount;
+        notify();
+        System.out.println("Deposit completed");
+    }
+
+    public synchronized void withdraw(int amount) {
+        if(amount > bal)
+        {
+            System.out.println("less bal");
+            try{
+                wait();
+            } catch(InterruptedException e) {}
+        }
+
+        bal -= amount;
+        System.out.println(amount + " with draw done");
+        System.out.println(bal + " is available bal");
+    }
+}
+
+
+public class InterThreadcommunication {
+    public static void main(String[] args) {
+        
+        Bank b = new Bank(2000);
+        new Thread() {
+            public void run() {
+                b.withdraw(3000);
+            }
+        }.start();
+        new Thread() {
+            public void run() {
+                b.deposit(5000);
+            }
+        }.start();
+    }
+    
+}
