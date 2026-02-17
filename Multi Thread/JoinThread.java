d8928fcb23d304745ae5e11de40c372983ae361e
Punith_Gs <152014411+punithgs@users.noreply.github.com>
2026-02-17 18:05:03 +0530

    Add JoinThread class to manage thread execution

--- Multi Thread/JoinThread.java
+++ Multi Thread/JoinThread.java
@@ -20,5 +20,22 @@ class Th2 extends Thread {
         }
     }
 }
+public class JoinThread {
+    static Th2 t2;
 
+    public static void main(String[] args) throws InterruptedException {
+
+        Th1 t1 = new Th1();
+        t2 = new Th2();   // assign to static variable
+
+        t1.start();
+        t2.start();
+
+        t1.join(); // main waits for Th1
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("Main : " + i);
+        }
+    }
+}
 
