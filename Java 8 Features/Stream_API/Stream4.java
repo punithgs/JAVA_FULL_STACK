a574b4f6e7ff8eba99964077b1cf62cd8951cbda
Punith_Gs <152014411+punithgs@users.noreply.github.com>
2026-03-12 09:26:32 +0530

    Update Stream4.java

--- Java 8 Features/Stream_API/Stream4.java
+++ Java 8 Features/Stream_API/Stream4.java
@@ -1,3 +1,11 @@
+/*
+Remove the duplicate values in to the array using stream API
+1) stream is used to convert the arrays into stream
+2) distinct are used to remove the duplicate values
+3) sorted are used to sort the given number in asending or sorted order
+4) toList are used to covert the stream into number(output format)
+5) output == [1, 2, 3, 4, 5, 6]
+*/
 
 import java.util.Arrays;
 import java.util.List;
