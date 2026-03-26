71f8584c01065f0aee518eccb76f7962494bcff9
Punith_Gs <152014411+punithgs@users.noreply.github.com>
2026-03-26 10:03:33 +0530

    Update Add Binary.java

--- Leetcode_String/Add Binary.java
+++ Leetcode_String/Add Binary.java
@@ -17,9 +17,6 @@ Output: "10101"
 
 Constraints:
 
-1 <= a.length, b.length <= 104
-a and b consist only of '0' or '1' characters.
-Each string does not contain leading zeros except for the zero itself.
 */
 
 class Solution {
