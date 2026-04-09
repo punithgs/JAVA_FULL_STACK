8e9b34a07096ae1df1b1b4f939ccf9e44153934f
Punith_Gs <152014411+punithgs@users.noreply.github.com>
2026-04-09 20:09:35 +0530

    Add files via upload

---
+++ Leetcode_String/Binary Tree Paths.java
@@ -0,0 +1,42 @@
+/*
+257. Binary Tree Paths
+
+Given the root of a binary tree, return all root-to-leaf paths in any order.
+
+A leaf is a node with no children.
+
+Example 1:
+
+Input: root = [1,2,3,null,5]
+Output: ["1->2->5","1->3"]
+Example 2:
+
+Input: root = [1]
+Output: ["1"]
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [1, 100].
+-100 <= Node.val <= 100
+*/
+public class Binary Tree Paths {
+    public List<String> binaryTreePaths(TreeNode root) {
+        List<String> result = new ArrayList<> ();
+        dfs (root, "", result);
+        return result;
+    }
+
+    public void dfs (TreeNode node, String path, List<String> result) {
+        if(node == null) return;
+
+        path += node.val;
+
+        if (node.left == null && node.right == null) {
+            result.add(path);
+            return;
+        }
+        dfs(node.left, path + "->", result);
+        dfs(node.right, path + "->", result);
+    }
+}
