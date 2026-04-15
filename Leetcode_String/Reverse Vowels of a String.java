8f7d291702049bff8576344e2f26fcbcbfbbdd12
Punith_Gs <152014411+punithgs@users.noreply.github.com>
2026-04-15 08:28:52 +0530

    Update Reverse Vowels of a String.java

--- Leetcode_String/Reverse Vowels of a String.java
+++ Leetcode_String/Reverse Vowels of a String.java
@@ -1,10 +1,6 @@
 /*
 345. Reverse Vowels of a String
-Solved
-Easy
-Topics
-premium lock icon
-Companies
+
 Given a string s, reverse only all the vowels in the string and return it.
 
 The vowels are 'a', 'e', 'i', 'o', and 'u', and they can appear in both lower and upper cases, more than once.
