9fbdebd522c833ac3b9bc1d40335cfca5aa8e2c2
Punith_Gs <152014411+punithgs@users.noreply.github.com>
2026-04-17 19:34:05 +0530

    Update Longest Common Prefix.java

--- Leetcode_String/Longest Common Prefix.java
+++ Leetcode_String/Longest Common Prefix.java
@@ -1,15 +1,10 @@
 /*
+
 14. Longest Common Prefix
-Solved
-Easy
-Topics
-premium lock icon
-Companies
+
 Write a function to find the longest common prefix string amongst an array of strings.
 
 If there is no common prefix, return an empty string "".
-
- 
 
 Example 1:
 
@@ -27,7 +22,9 @@ Constraints:
 1 <= strs.length <= 200
 0 <= strs[i].length <= 200
 strs[i] consists of only lowercase English letters if it is non-empty.
+
 */
+
 class Solution {
     public String longestCommonPrefix(String[] strs) {
         
