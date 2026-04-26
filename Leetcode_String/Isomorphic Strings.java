c0adcc0e29498f530814da642ac05d8510e6ee8a
Punith_Gs <152014411+punithgs@users.noreply.github.com>
2026-04-26 18:45:09 +0530

    Update Isomorphic Strings.java

--- Leetcode_String/Isomorphic Strings.java
+++ Leetcode_String/Isomorphic Strings.java
@@ -1,4 +1,5 @@
 /*
+
 205. Isomorphic Strings
 
 Given two strings s and t, determine if they are isomorphic.
@@ -6,8 +7,6 @@ Given two strings s and t, determine if they are isomorphic.
 Two strings s and t are isomorphic if the characters in s can be replaced to get t.
 
 All occurrences of a character must be replaced with another character while preserving the order of characters. No two characters may map to the same character, but a character may map to itself.
-
- 
 
 Example 1:
 
@@ -37,13 +36,12 @@ Input: s = "paper", t = "title"
 
 Output: true
 
- 
-
 Constraints:
 
 1 <= s.length <= 5 * 104
 t.length == s.length
 s and t consist of any valid ascii character.
+
 */
 
 class Solution {
