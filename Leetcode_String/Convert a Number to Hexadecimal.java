1ca392e5185db8d6788deb4ef2ddbab5baaea8d1
Punith_Gs <152014411+punithgs@users.noreply.github.com>
2026-05-02 18:36:14 +0530

    Update Convert a Number to Hexadecimal.java

--- Leetcode_String/Convert a Number to Hexadecimal.java
+++ Leetcode_String/Convert a Number to Hexadecimal.java
@@ -1,17 +1,12 @@
 /*
+
 405. Convert a Number to Hexadecimal
-Solved
-Easy
-Topics
-premium lock icon
-Companies
+
 Given a 32-bit integer num, return a string representing its hexadecimal representation. For negative integers, two’s complement method is used.
 
 All the letters in the answer string should be lowercase characters, and there should not be any leading zeros in the answer except for the zero itself.
 
 Note: You are not allowed to use any built-in library method to directly solve this problem.
-
- 
 
 Example 1:
 
@@ -22,11 +17,11 @@ Example 2:
 Input: num = -1
 Output: "ffffffff"
  
-
 Constraints:
 
 -231 <= num <= 231 - 1
 */
+
 class Solution {
     public String toHex(int num) {
         if (num == 0) return "0";
